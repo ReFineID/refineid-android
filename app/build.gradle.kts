54942c83dd673a314d7ba9edb7c7846002eecdd8
Petri Koistinen <petri.koistinen@refineid.fi>
2026-08-30 23:33:44 +0300

    Configure packaging to keep pre-stripped JNA native library symbols

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -189,6 +189,9 @@ android {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
+        jniLibs {
+            keepDebugSymbols += "**/libjnidispatch.so"
+        }
     }
 
     lint {
